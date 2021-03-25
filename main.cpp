dda56edf00472bcc8b725a81d174cc91350f0449
Matthew McCracken <mdm16c@acu.edu>
2021-03-25 09:18:38 -0500

    add my own cube

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 const int height = 500;
 const int width = 650;
 
+// debugging
+int rot = 0;
+
 // create a struct to hold the x and y values for points
 struct point {
 	double x;
@@ -24,6 +27,14 @@ void myInit(void)
 	gluOrtho2D(0.0, float(width), 0.0, float(height));// to be explained
 }
 
+void keyboard(unsigned char key,int x,int y)
+{
+	if(key==27) exit(1);
+	rot+=5;				// increment the rotation angle by 5 degrees
+	rot%=360;			// ... and keep it within 0-359
+	glutPostRedisplay();
+}
+
 // the redraw function
 void myDisplay(void)
 {
@@ -33,33 +44,48 @@ void myDisplay(void)
 	glLoadIdentity();		// clear modelview
 	glTranslatef( 0,-3,-8 );
 
-	glBegin(GL_QUADS);		// using quadrilaterals as primitive
-	// Front
-	  glColor3f( 0, 1, 0 );		// set colour to pure green
-	  glVertex3f( -1,  0,  1 );
-	  glVertex3f( -1,  2,  1 );
-	  glVertex3f(  1,  2,  1 );
-	  glVertex3f(  1,  0,  1 );
+	// debugging
+	glRotatef(rot,0,1,0);
 
-	// Back
-	  glColor3f( 0, 0.8, 0 );		// a slightly darker green for the back
-	  glVertex3f( -1,  0, -1 );
-	  glVertex3f(  1,  0, -1 );
-	  glVertex3f(  1,  2, -1 );
-	  glVertex3f( -1,  2, -1 );
+	glBegin(GL_LINES);
+		// back face
+		glVertex3i(0, 1, -1);
+		glVertex3i(1, 1, -1);
 
-	// Left side
-	  glColor3f( 0, 0.6, 0 );		// an even darker shade for the sides
-	  glVertex3f( -1,  0,  1 );
-	  glVertex3f( -1,  2,  1 );
-	  glVertex3f( -1,  2, -1 );
-	  glVertex3f( -1,  0, -1 );
+		glVertex3i(1, 1, -1);
+		glVertex3i(1, 0, -1);
 
-	// Right side
-	  glVertex3f(  1,  0,  1 );
-	  glVertex3f(  1,  0, -1 );
-	  glVertex3f(  1,  2, -1 );
-	  glVertex3f(  1,  2,  1 );
+		glVertex3i(1, 0, -1);
+		glVertex3i(0, 0, -1);
+
+		glVertex3i(0, 0, -1);
+		glVertex3i(0, 1, -1);
+
+		// front face
+		glVertex3i(0, 1, -2);
+		glVertex3i(1, 1, -2);
+
+		glVertex3i(1, 1, -2);
+		glVertex3i(1, 0, -2);
+
+		glVertex3i(1, 0, -2);
+		glVertex3i(0, 0, -2);
+
+		glVertex3i(0, 0, -2);
+		glVertex3i(0, 1, -2);
+
+		// connect sides
+		glVertex3i(1, 1, -2);
+		glVertex3i(1, 1, -1);
+
+		glVertex3i(0, 0, -2);
+		glVertex3i(0, 0, -1);
+
+		glVertex3i(0, 1, -2);
+		glVertex3i(0, 1, -1);
+
+		glVertex3i(1, 0, -2);
+		glVertex3i(1, 0, -1);
 	glEnd();
 
 	glutSwapBuffers();
@@ -77,6 +103,7 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(100, 150); // set the window position on the screen
 	glutCreateWindow("3D Transformations"); // open the screen window(with its exciting title)
 	glutDisplayFunc(myDisplay);     // register the redraw function
+	glutKeyboardFunc(keyboard);
 
 	glMatrixMode( GL_PROJECTION );
 	glLoadIdentity();
